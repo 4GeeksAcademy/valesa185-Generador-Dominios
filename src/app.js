1dac4864b1ebe071978a3486268aac760d4fb5f4
Valeria Salas <89941500+Valesa185@users.noreply.github.com>
2023-06-20 02:14:12 +0000

    Proyecto Completado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var extensions = [".com", ".net", ".us", ".io"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let h = 0; h < noun.length; h++) {
+      for (let k = 0; k < extensions.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[h] + extensions[k]);
+      }
+    }
+  }
+}
